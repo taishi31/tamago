e0479ea4559713f2ac02955bd93860e45302a715
森 泰志 <moritaishi@mori-no-MacBook-Air.local>
2018-12-15 18:25:05 +0900

    Initialize commit.

---
+++ Assets/creatureStatus.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class creatureStatus : MonoBehaviour {
+
+	private TextMesh targetText;
+	SpriteRenderer monsterRenderer;
+
+    int ageCount = 0;
+    float countTime = 0;
+	int time = 0;
+	int goodSleepTime = 0;
+	
+	bool wakeup = false;
+	bool isLightOn = false;
+	int hungryCount = 0;
+	int hungryCountMax = 4;
+	int hungryReduceTime = 0;
+	int foodCount = 0;
+
+	int stressCount = 0;
+	int stressCountMax = 4;
+	int stressIncreaseTime = 0;
+	
+	int unhappyCount = 0;
+
+	GameObject foodObject = null;
+	GameObject lightObject = null;
+	GameObject ageObject = null;
+	GameObject timeObject = null;
+	GameObject wakeupObject = null;
+	GameObject sleepObject = null;
+	GameObject hungryObject = null;
+	GameObject stressObject = null;
+	GameObject unhappyObject = null;
+
+	GameObject monsterObject = null;
+
+	public Sprite monster0Image;
+	public Sprite monster1Image;
+	public Sprite monster2Image;
+	public Sprite monster3Image;
+	public Sprite monster4Image;
+	public Sprite monster5Image;
+	public Sprite monster6Image;
+	public Sprite monster7Image;
+	public Sprite monster8Image;
+
+	// Use this for initialization
+	void Start () {
+		// initialize objects
+		foodObject = GameObject.Find("food");
+		lightObject = GameObject.Find("light");
+		ageObject = GameObject.Find("age");
+		timeObject = GameObject.Find("time");
+		wakeupObject = GameObject.Find("wakeup");
+		sleepObject = GameObject.Find("sleep");
+		hungryObject = GameObject.Find("hungry");
+		stressObject = GameObject.Find("stress");
+		unhappyObject = GameObject.Find("unhappy");
+
+		monsterObject = GameObject.Find("monster");
+		monsterRenderer = monsterObject.GetComponent<SpriteRenderer>();
+
+		updateAllText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		countTime += Time.deltaTime; 
+
+		// a time is one hour
+		if(countTime > 1.0f){
+			time++;
+
+			// 24hour is a day
+			if(time >= 24){
+				ageCount++;
+				targetText = ageObject.GetComponent<TextMesh>();
+				targetText.text = "年齢 " + ageCount.ToString("F0") + " 歳";
+
+				if(ageCount == 1){
+					// age 1
+					monsterRenderer.sprite = (foodCount > 3 ? monster1Image : monster2Image);
+				}
+				else if(ageCount == 3){
+					// age 3
+					monsterRenderer.sprite = (goodSleepTime > 20 ? monster3Image : monster4Image);
+				}
+				else if(ageCount == 5){
+					// age 5
+					monsterRenderer.sprite = (unhappyCount > 6 ? monster5Image : monster6Image);
+				}
+
+				time = 0;
+			}
+
+			targetText = timeObject.GetComponent<TextMesh>();
+			targetText.text = "Time " + time.ToString("F0") + ":00";
+
+			// creature is sleeping while 21:00 - 07:00
+			wakeup = !((time >= 21 && time < 24) || (time >= 0 && time < 7));
+
+			if(hungryCount > 0){
+				hungryReduceTime++;
+			}
+			if(wakeup == true){
+				stressIncreaseTime++;
+			}
+
+			targetText = wakeupObject.GetComponent<TextMesh>();
+			targetText.text = wakeup ? "起きてる" : "寝てる";
+
+			if(wakeup == false && isLightOn == false){
+				// good sleeping
+				goodSleepTime++;
+				targetText = sleepObject.GetComponent<TextMesh>();
+				targetText.text = "快眠 " + goodSleepTime.ToString("F0") + "ｈ";
+			}
+			
+			countTime = 0.0f;
+		}
+
+		// creature is hungry
+		if(hungryReduceTime >= 4){
+			if(hungryCount > 0){
+				hungryCount--;
+				updateHungryText(hungryCount);
+			}
+			hungryReduceTime = 0;
+		}
+
+		// creature get stress
+		if(stressIncreaseTime >= 5){
+			if(stressCount < stressCountMax){
+				stressCount++;
+				updateStressText(stressCount);
+			}
+			else if(stressCount >= stressCountMax){
+				unhappyCount++;
+
+				targetText = unhappyObject.GetComponent<TextMesh>();
+				targetText.text = "不満 " + unhappyCount.ToString() + " 回";
+			}
+			stressIncreaseTime = 0;
+		}
+	}
+
+	public void onClickPlayButton(){
+		if(wakeup){
+			if(stressCount > 0){
+				stressCount--;
+				updateStressText(stressCount);
+			}
+		}
+	}
+
+	public void onClickFoodButton () {
+		if(wakeup){
+			if(hungryCount < hungryCountMax){
+				hungryCount++;
+				updateHungryText(hungryCount);
+
+				foodCount++;
+				targetText = foodObject.GetComponent<TextMesh>();
+				targetText.text = "エサ" + foodCount.ToString() + "個";
+			}
+		}
+	}
+	
+	public void onClickLightButton(){
+		isLightOn = !isLightOn;
+		
+		targetText = lightObject.GetComponent<TextMesh>();
+		targetText.text = "電気 " + (isLightOn ? "ON" : "OFF");
+	}
+
+	public void onClickClearButton(){
+		time = 0;
+		ageCount = 0;
+		foodCount = 0;
+		hungryCount = 0;
+		stressCount = 0;
+		unhappyCount = 0;
+		goodSleepTime = 0;
+
+		isLightOn = false;
+		
+		monsterRenderer.sprite = monster0Image;
+
+		updateAllText();
+	}
+
+	private void updateHungryText(int hungryCount){
+		targetText = hungryObject.GetComponent<TextMesh>();
+		string hungryCountText = string.Empty;
+		int i = 0;
+		for(; i < hungryCount; i++){
+			hungryCountText += " ●";
+		}
+		for(; i < hungryCountMax; i++){
+			hungryCountText += " ○";
+		}
+		targetText.text = "お腹 " + hungryCountText;	
+	}
+
+	private void updateStressText(int stressCount){
+		targetText = stressObject.GetComponent<TextMesh>();
+		string stressCountText = string.Empty;
+		int i = 0;
+		for(; i < stressCount; i++){
+			stressCountText += " ●";
+		}
+		for(; i < stressCountMax; i++){
+			stressCountText += " ○";
+		}
+		targetText.text = "ストレス " + stressCountText;
+	}
+
+	private void updateAllText(){
+
+		targetText = timeObject.GetComponent<TextMesh>();
+		targetText.text = "Time " + time.ToString("F0") + ":00";
+
+		targetText = ageObject.GetComponent<TextMesh>();
+		targetText.text = "年齢 " + ageCount.ToString("F0") + " 歳";
+
+		targetText = foodObject.GetComponent<TextMesh>();
+		targetText.text = "エサ " + foodCount.ToString() + " 個";
+
+		targetText = lightObject.GetComponent<TextMesh>();
+		targetText.text = "電気 " + (isLightOn ? "ON" : "OFF");
+
+		targetText = unhappyObject.GetComponent<TextMesh>();
+		targetText.text = "不満 " + unhappyCount.ToString() + " 回";
+
+		targetText = sleepObject.GetComponent<TextMesh>();
+		targetText.text = "快眠 " + goodSleepTime.ToString("F0") + "ｈ";
+
+		updateHungryText(hungryCount);
+		updateStressText(stressCount);
+	}
+}
---
+++ Assets/testScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+		}
+	}
+}
