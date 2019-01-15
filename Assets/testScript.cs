00f440ddb9212d1bde33d5c4868651fccf787e6e
Taishi Mori <moritaishi@mori-no-MacBook-Air.local>
2019-01-16 00:54:28 +0900

    modified testScript.

--- Assets/testScript.cs
+++ Assets/testScript.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class testScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Translate (-0.1f, 0, 0);
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Translate ( 0.1f, 0, 0);
-		}
-	}
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            transform.Translate (-0.1f, 0, 0);
+        }
+        if (Input.GetKey (KeyCode.RightArrow)) {
+            transform.Translate ( 0.1f, 0, 0);
+        }
+    }
 }
